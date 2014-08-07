a7c391635c0cd528c5d783771f9e82dab542f810
Bart <dabipl@o2.pl>
2014-08-07 19:39:57 +0200

    First commit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+
+	module.exports = {
+		//url : 'mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu'
+		//url : 'mongodb://dabipl:QWEasdzxc123@ds063297.mongolab.com:63297/Todo'
+		url : 'mongodb://dabipl:QWEasdzxc123@kahana.mongohq.com:10094/dabipl'
+	};
